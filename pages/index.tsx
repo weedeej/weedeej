0bdca5b1a971f3ac43360fea6b911b9bb069d39d
Dixie J <devildjoseph666@gmail.com>
2022-04-18 06:03:57 +0800

    Portfolio

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ const Home: NextPage = () => {
   return (
     <>
       <div className='flex flex-row justify-center w-full p-10 bg-black text-white'>
-        Portfollio :D 
+        Portfolio :D 
       </div>
     </>
   )
